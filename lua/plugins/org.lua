062531c64bb8c215e30d0aced080db102295f4ff
SamuelLorrain <samuel.lorrain@gmail.com>
2024-05-12 12:57:26 +0200

    alter new org docs

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -3,17 +3,16 @@ return {
   event = 'VeryLazy',
   ft = { 'org' },
   config = function()
-    -- Setup orgmode
-    require('orgmode').setup({
-      org_agenda_files = '~/Orgmode/**/*',
-      org_default_notes_file = '~/Orgmode/refile.org',
-    })
-
-    -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
-    -- add `org` to ignore_install
-    -- require('nvim-treesitter.configs').setup({
-    --   ensure_installed = 'all',
-    --   ignore_install = { 'org' },
-    -- })
+        require('orgmode').setup({
+            org_agenda_files = '~/Orgmode/**/*',
+            org_default_notes_file = '~/Orgmode/refile.org',
+            org_startup_folded = 'showeverything',
+            org_capture_templates = {
+                t = { description = 'Todo', template = '* TODO %?\n %U'},
+                e = { description = 'Event', template = '* %?\n SCHEDULED: %T'},
+                x = { description = "Todo from clipboard", template = '* TODO %?\n %x\n %U'},
+                y = { description = "Note from clipboard", template = '* %?\n %x\n %u'}
+            }
+        })
   end,
 }
