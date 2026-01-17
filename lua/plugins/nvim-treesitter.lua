ad8a243b2e6d5fa6b0383190d3bee64849cfa246
SamuelLorrain <samuel.lorrain@gmail.com>
2026-01-17 13:49:00 +0100

    treesitter conf: dont ensure installed all ts grammars

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = "all",
             ignore_install = { 'org' },
             auto_install = false,
             highlight = {
