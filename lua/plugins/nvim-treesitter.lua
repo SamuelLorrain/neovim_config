f3a6be612f9927786ae56d8c65c3b1eb2ab08c09
SamuelLorrain <samuel.lorrain@carbonmaps.io>
2026-04-27 12:20:10 +0200

    improve treesitter ensure installed

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = "all",
+            ensure_installed = { "python", "javascript", "typescript", "c", "haskell"},
             ignore_install = { 'org' },
             auto_install = false,
             highlight = {
